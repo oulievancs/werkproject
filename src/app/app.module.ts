2ac43af0af3c1610b1cdd07374288fc4b3b46590
Evangelos Oulis <e.oulis@app-art.gr>
2024-01-07 16:45:48 +0200

    Init commit. add primeng library.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {NgModule} from "@angular/core";
+import {BrowserModule, provideClientHydration} from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
 
 @NgModule({
   declarations: [
@@ -17,4 +17,5 @@ import { AppComponent } from './app.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
